bd1b75ec9199e1e0cd6a116093be2640d3e33f46
Finn1226 <yifeili1226@gmail.com>
2026-03-06 09:33:51 -0500

    allow CORS from lihongfei.com

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["https://lihongfei.com", "http://lihongfei.com"],
+    allow_methods = ['GET'],
+    allow_headers = ['*']
+)
 
 @app.get('/')
 def get_root():
