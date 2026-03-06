7c114147511842b5c58c1ef7a4c670348f1c103b
Finn1226 <yifeili1226@gmail.com>
2026-03-06 11:56:47 -0500

    added a post request

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
+from pydantic import BaseModel
+from typing import Dict, List
+from psycopg2.extras import Json
 
+###
+### SETTING UP
+###
 app = FastAPI()
 
 app.add_middleware(
@@ -18,6 +24,9 @@ conn = psycopg2.connect(
     port = '5432'
 )
 
+###
+### GET
+###
 @app.get('/songs')
 def get_all_chords():
     cur = conn.cursor()
@@ -46,4 +55,29 @@ def get_chords(song_name: str):
     if result is None:
         return {'error': "song not found"}
     return result[0]
+
+###
+### POST
+###
+class SongCreate(BaseModel):
+    title: str
+    artist: str
+    chords: Dict[str, List[str]]
+
+@app.post('/songs')
+def add_song(song: SongCreate):
+    with conn.cursor() as cur:
+        cur.execute("""
+                    INSERT INTO songs (title, artist, chords)
+                    VALUES (%s, %s, %s)""",
+                    (song.title, song.artist, Json(song.chords))
+        )
+        conn.commit()
+
+    return {
+        'message': "Song created successfully",
+        'title': song.title,
+        'artist': song.artist,
+        'chords': song.chords
+    }
 
